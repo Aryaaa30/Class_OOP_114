a38aaa387af601a1d6466f80f56e0e37e146ffba
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-04-10 11:15:16 +0700

    Menambahkan Method input di class Mahasiswa

--- Class_OOP_114/Class_OOP_114.cpp
+++ Class_OOP_114/Class_OOP_114.cpp
@@ -8,42 +8,43 @@ public:
 public: void cetak() {
     cout << "NIM = " << nim;
     cout << "\nNama = " << nama;
-    } 
-};
-
-class Matakuliah {
-private:
-    string kode;
-    string namaM;
-    int sks;
-public:
-    void input() {
-        cout << "\nMasukkan kode Matakuliah : ";
-        cin >> kode;
-        cout << "\nMasukkan Nama Matakuliah : ";
-        cin >> namaM;
-        cout << "Jumlah SKS : ";
-        cin >> sks;
-    }
-    void tampil() {
-        cout << "Kode Matakuliah : " << kode;
-        cout << "\nNama Matakuliah : " << namaM;
-        cout << "\nSKS : " << sks;
-    }
-};
-
-int main()
-{
-    Mahasiswa mhs1;
-    Matakuliah mk;
-
-    cout << "Masukkan NIM : ";
-    cin >> mhs1.nim;
-    cout << "Masukkan Nama : ";
-    cin >> mhs1.nama;
-    mhs1.cetak();
-    mk.input();
-    mk.tampil();
 }
+public:void input() {
+    cout << "Masukkan NIM : ";
+    cin >> nim;
+    cout << "Masukkan Nama : ";
+    cin >> nama;
+};
 
+      class Matakuliah {
+      private:
+          string kode;
+          string namaM;
+          int sks;
+      public:
+          void input() {
+              cout << "\nMasukkan kode Matakuliah : ";
+              cin >> kode;
+              cout << "\nMasukkan Nama Matakuliah : ";
+              cin >> namaM;
+              cout << "Jumlah SKS : ";
+              cin >> sks;
+          }
+          void tampil() {
+              cout << "Kode Matakuliah : " << kode;
+              cout << "\nNama Matakuliah : " << namaM;
+              cout << "\nSKS : " << sks;
+          }
+      };
 
+      int main()
+      {
+          Mahasiswa mhs1;
+          Matakuliah mk;
+
+          mhs1.input();
+          mhs1.cetak();
+          mk.input();
+          mk.tampil();
+      }
+};
